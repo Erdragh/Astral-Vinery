c501ac7e7e6a7fe62b14bc0966d9305f9ce066fe
Jan Bayer <enderjanyt@gmail.com>
2023-03-19 17:01:06 +0100

    add wine press rei integration

--- src/main/java/com/github/erdragh/astralvinery/compat/rei/AstralVineryClientPlugin.java
+++ src/main/java/com/github/erdragh/astralvinery/compat/rei/AstralVineryClientPlugin.java
@@ -4,12 +4,16 @@ import com.github.erdragh.astralvinery.compat.rei.cooking_pot.CookingPotCategory
 import com.github.erdragh.astralvinery.compat.rei.cooking_pot.CookingPotDisplay;
 import com.github.erdragh.astralvinery.compat.rei.fermentation_barrel.FermentationBarrelCategory;
 import com.github.erdragh.astralvinery.compat.rei.fermentation_barrel.FermentationBarrelDisplay;
+import com.github.erdragh.astralvinery.compat.rei.wine_press.WinePressCategory;
+import com.github.erdragh.astralvinery.compat.rei.wine_press.WinePressDisplay;
 import com.github.erdragh.astralvinery.compat.rei.wood_fired_oven.WoodFiredOvenCategory;
 import com.github.erdragh.astralvinery.compat.rei.wood_fired_oven.WoodFiredOvenDisplay;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import satisfyu.vinery.recipe.CookingPotRecipe;
@@ -25,10 +29,12 @@ public class AstralVineryClientPlugin implements REIClientPlugin {
         registry.add(new CookingPotCategory());
         registry.add(new FermentationBarrelCategory());
         registry.add(new WoodFiredOvenCategory());
+        registry.add(new WinePressCategory());
 
         registry.addWorkstations(REICategories.COOKING_POT_CATEGORY, EntryStacks.of(ObjectRegistry.COOKING_POT));
         registry.addWorkstations(REICategories.FERMENTATION_BARREL_CATEGORY, EntryStacks.of(ObjectRegistry.FERMENTATION_BARREL));
         registry.addWorkstations(REICategories.WOOD_FIRED_OVEN_CATEGORY, EntryStacks.of(ObjectRegistry.WOOD_FIRED_OVEN));
+        registry.addWorkstations(REICategories.WINE_PRESS_CATEGORY, EntryStacks.of(ObjectRegistry.WINE_PRESS));
     }
 
     @Override
@@ -36,5 +42,6 @@ public class AstralVineryClientPlugin implements REIClientPlugin {
         registry.registerRecipeFiller(CookingPotRecipe.class, VineryRecipeTypes.COOKING_POT_RECIPE_TYPE, CookingPotDisplay::new);
         registry.registerRecipeFiller(FermentationBarrelRecipe.class, VineryRecipeTypes.FERMENTATION_BARREL_RECIPE_TYPE, FermentationBarrelDisplay::new);
         registry.registerRecipeFiller(WoodFiredOvenRecipe.class, VineryRecipeTypes.WOOD_FIRED_OVEN_RECIPE_TYPE, WoodFiredOvenDisplay::new);
+        registry.add(new WinePressDisplay());
     }
 }
--- src/main/java/com/github/erdragh/astralvinery/compat/rei/REICategories.java
+++ src/main/java/com/github/erdragh/astralvinery/compat/rei/REICategories.java
@@ -3,6 +3,7 @@ package com.github.erdragh.astralvinery.compat.rei;
 import com.github.erdragh.astralvinery.AstralVinery;
 import com.github.erdragh.astralvinery.compat.rei.cooking_pot.CookingPotDisplay;
 import com.github.erdragh.astralvinery.compat.rei.fermentation_barrel.FermentationBarrelDisplay;
+import com.github.erdragh.astralvinery.compat.rei.wine_press.WinePressDisplay;
 import com.github.erdragh.astralvinery.compat.rei.wood_fired_oven.WoodFiredOvenDisplay;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import net.minecraft.util.Identifier;
@@ -11,4 +12,5 @@ public class REICategories {
     public static final CategoryIdentifier<CookingPotDisplay> COOKING_POT_CATEGORY = CategoryIdentifier.of(new Identifier(AstralVinery.MODID, "cooking_pot"));
     public static final CategoryIdentifier<FermentationBarrelDisplay> FERMENTATION_BARREL_CATEGORY = CategoryIdentifier.of(new Identifier(AstralVinery.MODID, "fermentation_barrel"));
     public static final CategoryIdentifier<WoodFiredOvenDisplay> WOOD_FIRED_OVEN_CATEGORY = CategoryIdentifier.of(new Identifier(AstralVinery.MODID, "wood_fired_oven"));
+    public static final CategoryIdentifier<WinePressDisplay> WINE_PRESS_CATEGORY = CategoryIdentifier.of(new Identifier(AstralVinery.MODID, "wine_press"));
 }
