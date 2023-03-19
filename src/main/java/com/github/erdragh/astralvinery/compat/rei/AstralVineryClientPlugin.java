d51431f1a43d776c0a6ebd0b3761f147594d974e
Jan Bayer <enderjanyt@gmail.com>
2023-03-19 17:01:29 +0100

    remove unused imports

--- src/main/java/com/github/erdragh/astralvinery/compat/rei/AstralVineryClientPlugin.java
+++ src/main/java/com/github/erdragh/astralvinery/compat/rei/AstralVineryClientPlugin.java
@@ -11,9 +11,7 @@ import com.github.erdragh.astralvinery.compat.rei.wood_fired_oven.WoodFiredOvenD
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
-import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
-import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import satisfyu.vinery.recipe.CookingPotRecipe;
